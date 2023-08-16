84bb2d5d52249c6e76a0022bb093a42eaa397c00
Amal <amal_brahimi@yahho.com>
2023-08-16 17:33:27 -0400

    The

--- src/FireOne/FireTest1.java
+++ src/FireOne/FireTest1.java
@@ -4,6 +4,8 @@ public class FireTest1 {
 
 	public static void main(String[] args) {
 		System.out.println("Lake Tahoe");
+		System.out.println("Lake Tahoe");
+		System.out.println("Lake Tahoe");
 
 	}
 
